a408f88d648df338bfd44741057a0d7985d1d216
kuo22 <kuohong22@gmail.com>
2018-05-17 01:17:32 -0700

    Added functions

--- run_Guess_My_Number.py
+++ run_Guess_My_Number.py
@@ -5,21 +5,42 @@ def getRandomInt(a, b):
     return random.randint(a, b)
 
 def isPrimeUnder1000(m):
-    if (m > 1000 or m <= 0):
+    if (m > 1000 or m < 2):
         return False
-    for i in range(2, int(math.sqrt(m)) + 1):
+    for i in range(2, math.ceil((math.sqrt(m)))):
         if (m % i == 0):
             return False
     return True
 
-num = input("Enter a number: ")
-print(isPrimeUnder1000(int(num)))
+def is_n_minus_k_divisible_by_m(n, k, m):
+    return ((n - k) % m == 0)
 
-
-    
 def phaseOne():
-    return 1
+    option = int(input("1: Guess "))
+    switcher = {
+        1: guess
+    }
+    switcher.get(option, lambda: print("Invalid option"))()
 
 def phaseTwo():
     return 2
 
+def guess():
+    guess = input("what is your guess? ")
+    if (guess == secret_num):
+        print("You got it!")
+        playing = False
+    else:
+        print("That is wrong")
+
+playing = True
+score = 0
+turns = 0
+secret_num = getRandomInt(0, 1000)
+print(secret_num)
+while playing:
+    phaseOne()
+
+
+#num = input("Enter a number: ")
+#print(isPrimeUnder1000(int(num)))
